23b65c6d3b4f147a33c34fad321938cdab1faece
Jaden Johnson <jadenjohnson@fb.com>
2025-11-09 20:24:44 -0500

    change to json

--- agent/simple.py
+++ agent/simple.py
@@ -821,8 +821,8 @@ class SimpleAgent:
             current_player_coords = f"(X={player_coords[0]}, Y={player_coords[1]})"
             map_info = game_state.get('map', {}) or {}
             player_data = game_state.get('player', {}) or {}
-            map_only_sections = _format_map_info(map_info, player_data, include_npcs=True, full_state_data=game_state)
-            map_only = "\n".join(map_only_sections) if map_only_sections else ""
+            map_only_sections = _format_map_info(map_info, player_data, include_npcs=True, full_state_data=game_state, use_json_map=True)
+            #map_only = "\n".join(map_only_sections) if map_only_sections else ""
             player_location = game_state.get("player", {}).get("location", "Unknown Location")
             pathfinding_rules = ""
             if context != "title":
@@ -845,8 +845,8 @@ Your current objectives are:
 Your current location is:
 {player_location}
 
-Current map is:
-{map_only}
+Current map json is:
+{map_only_sections}
 
 Movement preview (check this to make sure you aren't selecting a blocked action):
 {map_preview}
