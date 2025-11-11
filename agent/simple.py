f507db95d685373066e2758c8a2b719a2785ed49
Jaden Johnson <jadenjohnson@fb.com>
2025-11-11 00:06:32 -0500

    update action prompt for warps

--- agent/simple.py
+++ agent/simple.py
@@ -1028,7 +1028,8 @@ liberally! Using it will also override any other actions you input, so don't inc
 When using the navigate_to(x,y) tool, make sure to only choose reachable tiles! This information is provided above. Lower X means the tile is to your west, while a lower Y means the tile is to your north.
 Some important notes: Don't use the navigate_to(x,y) tool when you are in dialogue or battle, you will be stuck.
 The navigate_to tool also doesn't contain information about NPCs, so if you are near an NPC and become stuck, you likely need to use single actions without the tool to sidestep them manually.
-IMPORTANT RULE: Don't interact with NPCs unless you have to. It will likely waste time.]
+IMPORTANT RULE: Don't interact with NPCs unless you have to. It will likely waste time.
+ALSO IMPORTANT: You interact with warps/stairs by walking into them. They will also show up in your movement preview. Confirm you are in front of them using your movement preview, then walk into them to transition.]
 
 ANALYSIS:
 [Summarize your current situation. This will be passed onto you as context during your next turn. It's especially important to summarize any dead ends you found and potential alternate paths. This is the only information that gets passed forward in time, so note anything important here. You can be as verbose as you like.]
