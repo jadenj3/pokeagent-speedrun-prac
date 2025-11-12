c8286c495359309e802a4761d67e4a6cf2e2de0f
Jaden Johnson <jadenjohnson@fb.com>
2025-11-11 20:00:57 -0500

    add some navigation tips

--- agent/simple.py
+++ agent/simple.py
@@ -1168,6 +1168,9 @@ Do not select a movement that is blocked. REMEMBER, BROWN LEDGES ARE BLOCKED!
 **IMPORTANT** To enter doors/stairs/warps CHECK THE MOVEMENT PREVIEW AND USE SINGLE ACTIONS. navigate_to is great for long distances, but it can struggle with entering locations if you are not perfectly aligned with the door or you are stuck on an NPC. The movement preview will have better information for you to use.
 If you want to you can use navigate_to to get close to the door, but afterwards make sure to use single actions.
 
+Navigation tips:
+- Enter and exit locations through the middle rather than hugging a specific side, this maximizes visibility of the area to locate the exit.
+
 In your response include the following sections:
 
 OBJECTIVES:
