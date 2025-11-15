bd89ebba4a0b99437d0fa0914ab4a9150a3e01b6
Jaden Johnson <jadenjohnson@fb.com>
2025-11-15 00:07:35 -0500

    fix flower shop description

--- agent/simple.py
+++ agent/simple.py
@@ -346,7 +346,7 @@ class SimpleAgent:
             },
             {
                 "id": "story_route_104_north",
-                "description": "Travel through northern section of Route 104 to Rustboro",
+                "description": "Travel through northern section of Route 104 to Rustboro. DO NOT go into the flower shop",
                 "objective_type": "location",
                 "target_value": "Route 104 North",
                 "milestone_id": "ROUTE_104_NORTH"
